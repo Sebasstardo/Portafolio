58437e19c6e96e1b489cafa5b0919172fe0bbaff
Sebasstardo <smiranda.ahumada@gmail.com>
2022-11-25 17:48:40 -0300

    trabajo en portafolio

--- src/js/main.js
+++ src/js/main.js
@@ -19,3 +19,4 @@ class navBar extends HTMLElement {
 }
 //se define primero el nombre con el cual se va a ocupar en el html, despues el nombre de la clase
 window.customElements.define("nav-bar", navBar);
+
